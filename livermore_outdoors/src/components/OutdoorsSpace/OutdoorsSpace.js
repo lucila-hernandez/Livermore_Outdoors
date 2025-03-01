ad24c6f8f2717842eace03c35bf092b52770b10f
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-02-28 23:32:08 -0800

    styled spaces

--- livermore_outdoors/src/components/OutdoorsSpace/OutdoorsSpace.js
+++ livermore_outdoors/src/components/OutdoorsSpace/OutdoorsSpace.js
@@ -6,11 +6,13 @@ function OutdoorsSpace({ id, name, address, image, hours }) {
   return (
     <div className="OutdoorsSpace">
       <Link to={`/details/${id}`}>
-        <h2>{name}</h2>
+        <h2 className="OutdoorsSpace-title">{name}</h2> {/* Add class name */}
         <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt={name} />
-        <p>{address}</p>
-        <p>{hours}</p>
       </Link>
+      <div className="OutdoorsSpace-info">
+        <div>{address}</div>
+        <div>{hours}</div>
+      </div>
     </div>
   );
 }
