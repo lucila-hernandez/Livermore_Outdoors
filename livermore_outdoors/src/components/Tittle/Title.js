975ed9e28ce5e5ded8619f999409e51b405502e7
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-03-01 19:42:56 -0800

    added random public space button

--- livermore_outdoors/src/components/Tittle/Title.js
+++ livermore_outdoors/src/components/Tittle/Title.js
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom'
 import React from 'react';
 import './Title.css';
+import RandomSpace from '../RandomSpace/RandomSpace'; 
+
 
 function Title() {
   return (
@@ -17,7 +19,7 @@ function Title() {
           className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
           to="/about">About</NavLink>
         </div>
-
+        <RandomSpace />
       </header>
     </div>
   )
