5ec6dbe41684a017614d5136004f28167160cdaf
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-03-05 21:43:56 -0800

    updated about page and made it the home page

--- livermore_outdoors/src/components/About/About.js
+++ livermore_outdoors/src/components/About/About.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './About.css';
 
 function About() {
   return (
-    <div>
-      <h1>About Livermore Outdoors</h1>
-      <p>Livermore Outdoors showcases the city's best parks, trails, and open spaces, offering scenic spots for hiking, picnicking, and recreation. 
-        From lush green parks to peaceful nature trails, these outdoor spaces provide a place for relaxation and adventure in the heart of Livermore.</p>
+    <div className="About">
+      <header className="About-Header">
+      <img src="/images/logo.png" alt="Livermore Outdoors" className="About-Image" />
+      </header>
+
+      <section className="About-Content">
+      <h2>About Livermore Outdoors</h2>
+        <p>🌲 Livermore Outdoors showcases the city's best parks, trails, and open spaces, offering scenic spots for 
+          hiking 🥾, picnicking 🧺, and recreation 🏕️.
+        </p>
+        <p>
+          From lush green parks 🌿 to peaceful nature trails 🚶‍♂️, these outdoor spaces provide a place for 
+          relaxation 🌄 and adventure 🚴‍♂️in the heart of Livermore.
+        </p>
+      </section>
+
+
+      <section className="About-Explore">
+        <h2>Start Exploring</h2>
+        <Link to="/">
+          <button className="explore-button">Explore Now</button>
+        </Link>
+      </section>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
