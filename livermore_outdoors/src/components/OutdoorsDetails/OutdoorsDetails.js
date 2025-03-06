0ea16694d75d46a503929c82948dcb08fcdd2318
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-03-06 10:42:07 -0800

    updated with map

--- livermore_outdoors/src/components/OutdoorsDetails/OutdoorsDetails.js
+++ livermore_outdoors/src/components/OutdoorsDetails/OutdoorsDetails.js
@@ -4,32 +4,41 @@ import data from '../../livermore-data.js'
 import './OutdoorsDetails.css'
 import OutdoorsFeatureList from '../OutdoorsFeatureList/OutdoorsFeatureList'
 
-
-
 function OutdoorsDetails(props) {
   const params = useParams()
   const { id } = params // Location index
   const { images, title, desc, hours, features, geo } = data[id]
+  const mapsUrl = `https://www.google.com/maps?q=${geo.lat},${geo.lon}&output=embed`;
 
   return (
     <div className="OutdoorsDetails">
+      <div className="OutdoorsDetails-info">
+        <h1 className="OutdoorsDetails-title">{title}</h1>
+        <p className="OutdoorsDetails-desc">{desc}</p>
+        <p className="OutdoorsDetails-hours">{hours}</p>
+
+        <OutdoorsFeatureList features={features} />
+      </div>
+
       <div className="OutdoorsDetails-image">
         <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title} />
       </div>
 
-      <div className ="OutdoorsDetails-info">
-        <h1 className="OutdoorsDetails-title">{ title }</h1>
-        <p className="OutdoorsDetails-desc">{ desc }</p>
-        <p className="OutdoorsDetails-hours">{ hours }</p>
-
-        <OutdoorsFeatureList features={features} />
-
-        <p className="OutdoorsDetails-geo">{geo.lat} {geo.lon}</p>
-
+      {/* Map section added below the image */}
+      <div className="OutdoorsDetails-mapSection">
+        <iframe
+          title={`Map of ${title}`}
+          src={mapsUrl}
+          width="100%"  // Make the map responsive
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
-
     </div>
   )
 }
 
-export default OutdoorsDetails
+export default OutdoorsDetails;
